ea9d60f1586584473695ddbaa0b8827427c15a96
domnititeodor <domnitit@gmail.com>
2021-10-20 13:53:05 +0300

    Laboratul 4 finalizat

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def show_menu():
     print('2. Verificare daca cele doua liste au acelasi numar de elemente pare')
     print('3. Intersectia celor doua multimi')
     print('4. Palindroamele obtinute prin concatenarea elementelor de pe aceeasi pozitie')
-    print('5. ')
+    print('5. Procesarea elementelor divizibile')
     print('x. Exit')
 
 
@@ -25,17 +25,26 @@ def read_set2():
     return set2
 
 
+def read_set3():
+    set3_as_str = input('Dati a treia multime: ')
+    set3_as_list_of_str = set3_as_str.split(' ')
+    set3 = []
+    for set3_str in set3_as_list_of_str:
+        set3.append(int(set3_str))
+    return set3
+
+
 def show_check_even(set1, set2):
     if count_evens(set1) == count_evens(set2):
         print(f"Multimile {set1} si {set2} au acelasi numar de elemente pare")
     else:
-        print("Multimile {set1} si {set2} nu au acelasi numar de elemente pare")
+        print(f"Multimile {set1} si {set2} nu au acelasi numar de elemente pare")
 
 
 def count_evens(set):
     """
     Determina numarul elementelor pare dintr-un sir
-    :param set: Lista de numere itnregi data spre verificare
+    :param set: Lista de numere intregi data spre verificare
     :return: Numarul de elemente pare din lista
     """
     e = 0
@@ -62,14 +71,11 @@ def show_intersection_sets(set1, set2):
 
 
 def is_palindrome(n):
-    '''
-    Determina daca un numar este palindrom(numar care scris invers este egal cu el insusi) sau nu
-    Input:
-    n, numar intreg, n >= 0
-    Output:
-    True, n palindrom
-    False, n nepalindrom
-    '''
+    """
+    Determina daca un numar este palindrom
+    :param n: numarul intreg dat spre verificare
+    :return: True, n palindrom / False, n nepalindrom
+    """
 
     copie = n
     oglindit = 0
@@ -102,6 +108,58 @@ def show_concatanated_palindromes(set1, set2):
         print(f"Palindroamele rezultate sunt {result}")
 
 
+def divisible_by_set3(x, set3):
+    """
+    Determina daca un numar este divizibil cu toate elementele dintr-o lista data
+    :param x: Numarul dat spre verificare
+    :param set3: Lista cu elemente date spre verificare
+    :return: True, daca numarul este divizibil cu toate elementele din lista / False, in caz contrar
+    """
+
+    divisible = True
+    for i in range(len(set3)):
+        if x % set3[i] != 0:
+            divisible = False
+            break
+    return divisible
+
+
+def mirror_int(n):
+    """
+    Determina oglinditul unui numar intreg dat
+    :param n: n, numar intreg
+    :return: Oglinditul numarului n
+    """
+    mirror = 0
+    while (n != 0):
+        mirror = mirror * 10 + n % 10
+        n = n // 10
+    return mirror
+
+
+def test_mirror_int():
+    assert mirror_int(22) == 22
+    assert mirror_int(347) == 743
+    assert mirror_int(2) == 2
+
+
+def show_mirrored_lists(set1, set2, set3):
+    result1 = []
+    result2 = []
+    for i in range(len(set1)):
+        if divisible_by_set3(set1[i], set3):
+            result1.append(mirror_int(set1[i]))
+        else:
+            result1.append(set1[i])
+    for i in range(len(set2)):
+        if divisible_by_set3(set2[i], set3):
+            result2.append(mirror_int(set2[i]))
+        else:
+            result2.append(set2[i])
+    print(result1)
+    print(result2)
+
+
 def main():
     lst = []
     while True:
@@ -116,6 +174,9 @@ def main():
             show_intersection_sets(set1, set2)
         elif option == '4':
             show_concatanated_palindromes(set1, set2)
+        elif option == '5':
+            set3 = read_set3()
+            show_mirrored_lists(set1, set2, set3)
         elif option == 'x':
             break
         else:
@@ -125,4 +186,5 @@ def main():
 if __name__ == '__main__':
     test_count_evens()
     test_is_palindrome()
+    test_mirror_int()
     main()
